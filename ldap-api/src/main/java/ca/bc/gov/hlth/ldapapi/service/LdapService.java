51f7030c34cf1938cff9fb7ed62754758f031bcb
Camille <camille.estival@cgi.com>
2021-08-23 16:21:52 -0700

    add remainingAttempts and  in api reponse

--- ldap-api/src/main/java/ca/bc/gov/hlth/ldapapi/service/LdapService.java
+++ ldap-api/src/main/java/ca/bc/gov/hlth/ldapapi/service/LdapService.java
@@ -46,12 +46,31 @@ public class LdapService {
         SearchResult userInfo = searchUser(userCredentials.getUserName());
         boolean userUnlocked = checkUserLocked(userInfo.getAttributes());
         boolean userPasswordExpired = checkUserPasswordExpired(userInfo.getAttributes());
+
         boolean validCredentials = false;
         if (userUnlocked) {
             validCredentials = authenticateUser(userInfo.getName(), userCredentials.getPassword());
         }
 
-        return createReturnMessage(userInfo.getName(), validCredentials, userUnlocked, userPasswordExpired, userInfo.getAttributes());
+        // Get nbLoginAttempts and lockoutTimeOut after authenticateUser 
+        long lockoutTimeInHours = 0;
+        int remainingAttempts = 3;
+        if (!validCredentials){
+            LoginAttempts loginAttemptsForUser = loginAttemptsMap.get(userInfo.getName());
+            if (loginAttemptsForUser != null){
+                remainingAttempts = 3 - loginAttemptsForUser.getAttempts();
+             
+                long hoursSinceLastAttempt = ChronoUnit.HOURS.between(loginAttemptsForUser.getLastAttempt(), LocalDateTime.now());
+                lockoutTimeInHours = attemptTimeout - hoursSinceLastAttempt;
+           
+            // Should never happen
+//           if (hoursSinceLastAttempt > attemptTimeout){
+//               lockoutTimeInHours = 0;
+//            }
+            }
+        }
+        
+        return createReturnMessage(userInfo.getName(), validCredentials, userUnlocked, userPasswordExpired, lockoutTimeInHours, remainingAttempts, userInfo.getAttributes());
     }
 
     private SearchResult searchUser(String username) {
@@ -107,6 +126,27 @@ public class LdapService {
         return expired;
     }
 
+    // TODO
+    private int getNbLoginAttempts(Attributes attributes) {
+        Attribute passwordChangeDate = attributes.get(LDAP_ATTR_PASSWORD_CHANGE_DATE);
+        Attribute passwordLifespan = attributes.get(LDAP_ATTR_PASSLIFESPAN);
+        boolean expired = false;
+
+        if (passwordChangeDate != null) {
+            try {
+                long epochDateLastChangedOn = Long.parseLong(passwordChangeDate.get().toString());
+                long today = LocalDate.now().toEpochDay();
+                expired = (epochDateLastChangedOn + retrievePasswordLifespan(passwordLifespan)) < today;
+            } catch (NamingException | NoSuchElementException e) {
+                /* If we can't get the expired password info we consider expired=false
+                 This is not actually used as part of determining a successful authentication it just suggests for a user
+                 to change their password */
+                webClientLogger.debug(e.getMessage());
+            }
+        }
+        return 1;
+    }
+        
     private int retrievePasswordLifespan(Attribute passwordLifespan) throws NamingException {
         int lifespan = 42; // Standard password lifespan setting from LDAPAdmin webapp
         if (passwordLifespan != null) {
@@ -152,7 +192,7 @@ public class LdapService {
         long hoursSinceLastAttempt = ChronoUnit.HOURS.between(loginAttemptsForUser.getLastAttempt(), LocalDateTime.now());
         int currentAttempts = loginAttemptsForUser.getAttempts();
         // Entry exists AND timestamp >1hr: set attempts=1, timestamp=now
-        if (hoursSinceLastAttempt > attemptTimeout) {
+        if (hoursSinceLastAttempt > attemptTimeout) { // reset the nb of attempts if the attemptsTimeout has passed
             loginAttemptsForUser.setAttempts(1);
             loginAttemptsForUser.setLastAttempt(LocalDateTime.now());
 
@@ -166,7 +206,8 @@ public class LdapService {
     }
 
     protected User createReturnMessage(String userName, boolean validCredentials, boolean userUnlocked,
-                                       boolean passwordExpired, Attributes attributes) {
+                                       boolean passwordExpired, long lockoutTimeInHours, int remainingAttempts,
+                                       Attributes attributes) {
 
         User userToReturn = new User();
         userToReturn.setUsername(userName);
@@ -185,11 +226,14 @@ public class LdapService {
                     e.printStackTrace(); // TODO we should return a 500 here
                 }
             }
+        } else if (!validCredentials) {
+            userToReturn.setLockoutTimeInHours(lockoutTimeInHours);
+            userToReturn.setRemainingAttempts(remainingAttempts);
         }
 
         return userToReturn;
     }
-
+        
     protected void lockUserAccount(String userInfoName) {
         try {
             DirContext ctx = new InitialDirContext(ldapProperties);
